552e3de21e1adf3ead168c6561a40262971f2bea
mrdvv <vilius.grumovskis@gmail.com>
2024-05-08 02:27:39 +0300

    todo dropdown

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+
 function Todo() {
-    return (
-        <div className="w-[49rem] h-[61rem] mx-auto mt-12 rounded-lg shadow-lg bg-todo">
-        <div className="w-full p-5">
-          <p className="font-semibold text-3xl flex justify-center">TODO LISTAS</p>
-          <button type="Dropdown" className="ml-12 bg-mainbg w-auto "></button>
-          </div>
-          <button type="submit" className="ml-12 text-white bg-mainBg font-medium rounded-lg text-lg w-auto px-10 py-2.5 text-center">Add Task</button>
-        
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  return (
+    <div className="w-[49rem] h-[61rem] mx-auto mt-12 rounded-lg shadow-lg bg-todo">
+      <div className="w-full p-5 flex justify-between items-center">
+        <p className="font-semibold text-3xl">TODO LISTAS</p>
+        <div className="flex items-center">
+          {dropdownOpen && (
+            <div className="absolute bg-white shadow-md p-2 top-10 right-0 w-48">
+              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2">
+                Download task list
+              </button>
+              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2">
+                Delete account
+              </button>
+            </div>
+          )}
+          <button onClick={toggleDropdown} type="button" className="w-8 h-8 text-white bg-mainBg font-medium rounded-lg text-lg px-10 py-2.5 text-center">
+            <img src="pics/settings.png" alt="Settings" />
+          </button>
         </div>
-      )
+      </div>
+      <button type="submit" className="ml-12 text-white bg-mainBg font-medium rounded-lg text-lg w-auto px-10 py-2.5 text-center">
+        Add Task
+      </button>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
