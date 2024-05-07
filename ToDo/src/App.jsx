67e39a6e05dfb607fc76c1d076e878c08c5e019e
DanVolu <Dan.Voluj@gmail.com>
2024-05-07 13:25:45 +0300

    my first commit

---
+++ ToDo/src/App.jsx
@@ -0,0 +1,20 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Login></Login>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter> */}
+    </>
+  )
+}
+
+export default App
---
+++ ToDo/src/components/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+ 
+
+  return (
+    <>
+        <p>!!!</p>
+    </>
+  )
+}
+
+export default Login
