edf193a7dc3bee533924e77e46ea8d9fa498f506
mrdvv <vilius.grumovskis@gmail.com>
2024-05-13 16:23:35 +0300

    backas pasetupintas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,12 +8,12 @@ function Todo() {
   };
 
   return (
-    <div className="w-[49rem] h-[61rem] mx-auto mt-12 rounded-lg shadow-lg bg-todo">
-      <div className="w-full p-5 flex justify-between items-center">
-        <p className="font-semibold text-3xl">TODO LISTAS</p>
-        <div className="flex items-center">
+    <div className="w-[48rem] h-[61rem] mx-auto mt-12 rounded-lg shadow-lg bg-todo">
+      
+        <p className="font-semibold text-4xl text-center">TODO LISTAS</p>
+        <div className="relative bg-mainBg flex h-[6rem] w-[6rem] items-center justify-items-end">
           {dropdownOpen && (
-            <div className="absolute bg-white shadow-md p-2 top-10 right-0 w-48">
+            <div className="absolute bg-white shadow-md p-2 top-10 right-0 w-12 z-10">
               <button className="block w-full text-left hover:bg-gray-100 px-4 py-2">
                 Download task list
               </button>
@@ -22,12 +22,12 @@ function Todo() {
               </button>
             </div>
           )}
-          <button onClick={toggleDropdown} type="button" className="w-8 h-8 text-white bg-mainBg font-medium rounded-lg text-lg px-10 py-2.5 text-center">
-            <img src="pics/settings.png" alt="Settings" />
+          <button onClick={toggleDropdown} type="button" className="justify-items-end text-white bg-mainBg shadow-md font-medium rounded-lg text-md h-[6rem] w-[6rem] text-center">
+            Dropdown
           </button>
         </div>
-      </div>
-      <button type="submit" className="ml-12 text-white bg-mainBg font-medium rounded-lg text-lg w-auto px-10 py-2.5 text-center">
+      
+      <button type="addTask" className="ml-12 text-white bg-mainBg font-medium rounded-lg text-lg w-auto px-10 py-2.5 text-center">
         Add Task
       </button>
     </div>
