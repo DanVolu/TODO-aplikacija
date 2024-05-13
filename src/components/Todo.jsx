1a502483755d38c0cb0dcb84d3bab6885474a50a
mrdvv <vilius.grumovskis@gmail.com>
2024-05-13 16:55:24 +0300

    TODO WIP

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,14 +10,15 @@ function Todo() {
   return (
     <div className="w-[48rem] h-[61rem] mx-auto mt-12 rounded-lg shadow-lg bg-todo">
       
-        <p className="font-semibold text-4xl text-center">TODO LISTAS</p>
-        <div className="relative bg-mainBg flex h-[6rem] w-[6rem] items-center justify-items-end">
+      <p className="font-semibold text-4xl text-center">TODO LISTAS</p>
+      
+        <div id="dropdownBtn" className="absolute bg-mainBg h-[4rem] w-[4rem] border">
           {dropdownOpen && (
-            <div className="absolute bg-white shadow-md p-2 top-10 right-0 w-12 z-10">
-              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2">
+            <div id="dropdown" className="absolute bg-mainBg shadow-md p-2 top-20 right-0 z-10">
+              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2 border">
                 Download task list
               </button>
-              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2">
+              <button className="block w-full text-left hover:bg-gray-100 px-4 py-2 border">
                 Delete account
               </button>
             </div>
@@ -25,8 +26,7 @@ function Todo() {
           <button onClick={toggleDropdown} type="button" className="justify-items-end text-white bg-mainBg shadow-md font-medium rounded-lg text-md h-[6rem] w-[6rem] text-center">
             Dropdown
           </button>
-        </div>
-      
+      </div>
       <button type="addTask" className="ml-12 text-white bg-mainBg font-medium rounded-lg text-lg w-auto px-10 py-2.5 text-center">
         Add Task
       </button>
